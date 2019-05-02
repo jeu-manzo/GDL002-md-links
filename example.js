e2988666a1143138fbd6d24de55c2aa60961d5be
Lourdes Jeunette Manzo Ramírez <jeunettemanzo@gmail.com>
2019-05-02 01:54:04 -0500

    refactor functions to promises

--- example.js
+++ example.js
@@ -1,15 +1,11 @@
-const mdLinks = require('./mdlinks');
+const mdLinks = require('./index.js');
 
-// test('pass validation single file', () => {
-//     const path = './path/to/my/file'
-//     const validation = mdLinks.validate(path)
 
-//     expect(validation).toBe(true)
-// })
 
-const file = 'mdLinks.js';
+const file = 'index.js';
 const directory = './test';
 const non = 'djdjdjd.js';
 
-mdLinks.statusPath(file);
-// mdLinks.readFile(file);
+mdLinks(directory)
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+
+
+//Verificar estatus del path
+function statusPath (path) {
+    return new Promise((resolve, reject) => {
+        fs.lstat(path, (err, stats) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(stats)
+        });
+    })
+}
+
+//Lee todos los archivos de un directorio
+function readDirectory (directory) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(directory, (err, data) => {
+            if (err)
+              reject(err);
+
+           resolve(data)
+        });
+    })
+}
+
+//Recibe un array y busca el archivo con extension md
+function findExtMd (files) {
+    return new Promise(function(resolve, reject) {
+        files.forEach(function(file) {
+            const ext = path.extname(file);
+            if (ext === '.md') {
+                resolve(file);
+            }
+        });
+    });
+}
+
+//Solo lee archivos (cualquier extension)
+function readFile (file) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err)
+                reject(err);
+            const dataString = data.toString();
+            resolve(dataString);
+        })
+    })
+}
+
+
+async function mdLinks (path) {
+    try {
+        const stats = await statusPath(path)
+        let result = null
+
+        if (stats.isDirectory()) {
+            const readDir = await readDirectory(path);
+            const extMd = await findExtMd(readDir);
+            const fileConvert = path + '/' + extMd;
+            const bla = await readFile(fileConvert)
+            result = bla;
+        } else {
+            result = await readFile(path)
+        }
+
+        return result
+    } catch (err) {
+        console.log('error', err)
+    }
+}
+
+module.exports = mdLinks;
